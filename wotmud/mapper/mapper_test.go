3a823e0bf6a16dcce5bf7c6272a250c4ad60fd20
Wil Hunt <huntwj@gmail.com>
2017-11-01 20:58:52 -0700

    Stub mapper files

---
+++ wotmud/mapper/mapper_test.go
@@ -0,0 +1,10 @@
+package mapper_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRoomEvent(t *testing.T) {
+	fmt.Println("Receiving lines with an observed at the end should result in a room observation event.")
+}
