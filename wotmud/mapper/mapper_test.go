c25bf4a82833fb35297ad4b03dbb2057a3d5fa40
Wil Hunt <huntwj@gmail.com>
2017-11-09 05:13:12 -0800

    Cleanup placeholder test in mapper package

--- wotmud/mapper/mapper_test.go
+++ wotmud/mapper/mapper_test.go
@@ -1,10 +1,9 @@
 package mapper_test
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestRoomEvent(t *testing.T) {
-	fmt.Println("Receiving lines with an observed at the end should result in a room observation event.")
+	t.Skip("Receiving lines with an observed at the end should result in a room observation event.")
 }
